32b529d8868090d22d75284f6b29b21d27e15ab5
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2023-11-18 19:37:36 +0100

    enh: auth from CLI flags

--- cmd/root.go
+++ cmd/root.go
@@ -2,19 +2,16 @@ package cmd
 
 import (
 	"github.com/notdodo/IAMme-IAMme/pkg/infra/neo4j"
-	"github.com/notdodo/IAMme-IAMme/pkg/infra/okta"
 	"github.com/notdodo/IAMme-IAMme/pkg/io/logging"
 
 	"github.com/spf13/cobra"
 )
 
-type clientsType struct {
-	okta  okta.OktaClient
-	neo4j neo4j.Neo4jClient
-}
-
-var clients *clientsType
 var logger logging.LogManager
+var verbose bool
+var orgUrl string
+var oktaClientToken string
+var neo4jClient neo4j.Neo4jClient
 var rootCmd = &cobra.Command{
 	Use:   "iamme",
 	Short: "A CLI tool to interact with Okta and Neo4j",
@@ -22,13 +19,13 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	logger = logging.NewLogManager()
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().StringVarP(&orgUrl, "org-url", "u", "", "Okta Org URL")
+	rootCmd.PersistentFlags().StringVarP(&oktaClientToken, "client-token", "c", "", "Okta Client Token")
 }
 
-func Execute(oktaClient okta.OktaClient, neo4jClient neo4j.Neo4jClient) {
-	clients = &clientsType{
-		oktaClient,
-		neo4jClient,
-	}
+func Execute(neo4j neo4j.Neo4jClient) {
+	neo4jClient = neo4j
 	if err := rootCmd.Execute(); err != nil {
 		logger.Error("Error executing command", "err", err)
 	}
